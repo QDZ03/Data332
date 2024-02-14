54058e3c3a16abfc50445282ebfdc012708e227a
QDZ03 <159860533+QDZ03@users.noreply.github.com>
2024-02-13 21:42:37 -0600

    Update Chapter 1.R

--- Chapter 1.R
+++ Chapter 1.R
@@ -104,13 +104,13 @@ sum(dice)
 roll()
 
 #Create dice with arguments
-roll2 <- function(bones <- 1:6) { 
+roll2 <- function(bones) { 
 die <- 1:6
 dice <- sample(bones, size = 2, replace = TRUE)
 sum(dice)
 }
 
-roll2()
+roll2(bones = 1:6)
 
 
 
