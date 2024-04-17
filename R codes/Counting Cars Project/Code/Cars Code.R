70c72c9e8f60f298aeb257e2f7412f3d1072aca6
QDZ03 <159860533+QDZ03@users.noreply.github.com>
2024-04-17 13:10:54 -0500

    Update Cars Code.R

--- R codes/Counting Cars Project/Code/Cars Code.R
+++ R codes/Counting Cars Project/Code/Cars Code.R
@@ -4,14 +4,7 @@ library(readxl)
 library(dplyr)
 library(lubridate)
 library(DT)
-
-setwd('C:/Users/quang/Downloads/Documents/Data 332/R-programming/Cars Project/Data')
-counting_cars <- read_excel('counting_cars.xlsx')
-
-# Subset the dataframe
-df_cars <- counting_cars[, c("Date", "MPH", "Time", "Temp", "Weather")]
-df_cars$Time <- as.POSIXct(df_cars$Time, format = "%H:%M:%S")
-df_cars$Hour <- hour(df_cars$Time)
+#setwd('C:/Users/quang/Downloads/Documents/Data 332/R-programming/Cars Project')
 
 # Define UI
 ui <- fluidPage(
@@ -32,12 +25,21 @@ ui <- fluidPage(
 
 # Define server
 server <- function(input, output) {
-  
+  cleaned_data <- reactive({
+    counting_cars <- read_excel('counting_cars.xlsx')
+    # Subset the dataframe
+    df_cars <- counting_cars[, c("Date", "MPH", "Time", "Temp", "Weather")]
+    df_cars$Time <- as.POSIXct(df_cars$Time, format = "%H:%M:%S")
+    df_cars$Hour <- hour(df_cars$Time)
+    return(df_cars)
+  })
   output$plot <- renderPlot({
+    req(input$plot_type)
+    df <- cleaned_data()
     # Check if the user selected "Density Plot of MPH by Weather"
     if (input$plot_type == "Density Plot of MPH by Weather") {
       # Create a density plot of MPH by Weather
-      ggplot(df_cars, aes(x = MPH, fill = Weather)) +
+      ggplot(df, aes(x = MPH, fill = Weather)) +
         geom_density(alpha = 0.5) + # Add density plot with transparency
         labs(title = "Density Plot of MPH by Weather Condition",
              x = "MPH", y = "Density", fill = "Weather") +
@@ -47,7 +49,7 @@ server <- function(input, output) {
     # Check if the user selected "Boxplot of MPH by Hour"
     else if (input$plot_type == "Boxplot of MPH by Hour") {
       # Create a boxplot of MPH by Hour with weather grouping
-      ggplot(df_cars, aes(x = as.factor(Hour), y = MPH)) +
+      ggplot(df, aes(x = as.factor(Hour), y = MPH)) +
         geom_boxplot(aes(fill = Weather)) + # Add boxplot with weather grouping   
         stat_summary(fun = mean, geom = "point", shape = 23, size = 4, color = "black") + # Add mean points
         stat_summary(fun = mean, geom = "text", aes(label = round(after_stat(y), digits = 2)), vjust = -1) + # Add mean text label
